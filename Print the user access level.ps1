a026157744131f1fadb82992b8398ef6bd45530d
dianacohen <ac171@walla.com>
2023-04-27 15:45:19 +0300

    added Print the user access level.ps1

---
+++ Print the user access level.ps1
@@ -0,0 +1,50 @@
+﻿#You can print the user's access level by using this PowerShell script.
+#In the output It will show you all the organizations name, but you will see the user's details in the org the has a username at.
+
+# $token (PAT) - the Personal Access Tokens need to be filled from this URL: "https://dev.azure.com/%YOUR_ORG%/_usersSettings/tokens"
+$token = "PAT” 
+ 
+$url="https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=6.0"
+
+$token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($token)"))
+
+$response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Basic $token"} -Method Get -ContentType application/json
+
+$publicAlias = $response.publicAlias
+
+# $displayName - the user's Display Name need to be filled from this URL: "https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users"
+$displayName = "Display Name"
+
+ 
+$url1=“https://app.vssps.visualstudio.com/_apis/accounts?memberId=$($publicAlias)&api-version=6.0”
+
+$response1 = Invoke-RestMethod -Uri $url1 -Headers @{Authorization = "Basic $token"} -Method Get -ContentType application/json
+
+Foreach($organization in $response1.value.accountName)
+{
+
+#Adding a space between each organization
+  echo ""
+  echo $organization
+
+  
+  $url2="https://vsaex.dev.azure.com/$($organization)/_apis/userentitlements?"+"`$filter"+"=name eq '$displayName'&api-version=6.0-preview.3"
+
+  $response2 = Invoke-RestMethod -Uri $url2 -Headers @{Authorization = "Basic $token"} -Method Get -ContentType application/json
+
+# Prints the user access level
+  Foreach($users in $response2.members)
+  {
+    if($users.user.displayName -eq "$displayName")
+    { 
+       echo $users.user.displayname 
+       $userID = $users.id 
+       $url3="https://vsaex.dev.azure.com/$($organization)/_apis/userentitlements/$($userID)?api-version=6.0-preview.3"
+  
+       $response3 = Invoke-RestMethod -Uri $url3 -Headers @{Authorization = "Basic $token"} -Method Get -ContentType application/json-patch+json
+       $response3.accessLevel.licenseDisplayName
+    }
+   
+  }
+
+}
